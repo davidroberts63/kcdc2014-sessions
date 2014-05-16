98a136637fb784da904e85b29a37e7a0b645aabb
David Roberts <david.roberts63@gmail.com>
2014-05-15 20:35:53 -0500

    Initial pages

---
+++ coursedata.js
@@ -0,0 +1,38 @@
+/* going: yes | no | maybe */
+
+/*
+	title_id and time_id are set later on using a function in the document ready code below.
+*/
+var fridayCourses = [
+	{
+		'course_title': 'Introduction to MongoDB',
+		'author': 'Bryan Nehl',
+		'description': 'A high level introduction and demonstration of MongoDB. Presentation of what MongoDB is, its features, strengths and why people are using it.',
+		'going': '',
+		'time':'8:30am-9:30am'
+	},
+	{
+		'course_title': 'Something else',
+		'author': 'John Doe',
+		'description': 'Some longer description fo the course',
+		'going': '',
+		'time':'9:40am-10:40am'
+	}
+];
+
+var saturdayCourses = [
+    {
+        'course_title': 'Exploring the Javascript Multivers',
+        'author': 'Bryan Nehl',
+        'description': 'A high level introduction and demonstration of MongoDB. Presentation of what MongoDB is, its features, strengths and why people are using it.',
+        'going': '',
+        'time':'8:30am-9:30am'
+    },
+    {
+        'course_title': 'Another else',
+        'author': 'John Doe',
+        'description': 'Some longer description fo the course',
+        'going': '',
+        'time':'9:40am-10:40am'
+    }
+];
